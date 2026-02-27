c47ab3182a343888807c8acf054b50eef0126659
amaramar34018-ctrl <amaramar34018@gmail.com>
2026-02-27 08:02:54 +0530

    Update app.py

--- mytravels - Copy/app.py
+++ mytravels - Copy/app.py
@@ -692,8 +692,9 @@ def book_package():
 # END OF NEW ROUTE
 # ==========================================
 if __name__ == '__main__':
-    init_db()  # Database update
-    # Inga mathavum:
-    app.run(host='0.0.0.0', port=10000)
+    init_db()
+    # Render port configuration
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 
